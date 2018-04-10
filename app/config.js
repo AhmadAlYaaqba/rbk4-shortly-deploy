998e579469748525cb55817f1edd4c24618ca2f0
Taloamri <ammarhalbouni@gmail.com>
2018-04-10 15:24:30 +0300

    user authenication work, still no HASH , Links not work

--- app/config.js
+++ app/config.js
@@ -1,4 +1,98 @@
 var path = require('path');
+var mongoose = require('mongoose');
+var url = "mongodb://localhost/shortlydb";
+mongoose.connect(url);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function(){
+  console.log("Connected");
+})
+
+
+
+
+ module.exports = db; 
+// db.knex.schema.hasTable('urls').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('urls', function (link) {
+//       link.increments('id').primary();
+//       link.string('url', 255);
+//       link.string('baseUrl', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// db.createConnection(url, function(error,dbs) {
+//   if (error){
+//     throw error; 
+//     console.log("inside")
+//   } 
+//   console.log(dbs)
+// });
+
+// exports.Users = db.Schema({
+//   id: db.Schema.Types.ObjectId,
+//   username: {
+//     type: String,
+//     required: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   }
+// });
+// exports.Users = db.Schema({
+//   id: db.Schema.Types.ObjectId,
+//   username: {
+//     type: String,
+//     required: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   }
+// });
+
+
+// module.exports = db;
+
+/*
+
+// });// db.knex.schema.hasTable('urls').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('urls', function (link) {
+//       link.increments('id').primary();
+//       link.string('url', 255);
+//       link.string('baseUrl', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://../db/mongDB";
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Database created!");
+//   db.close();
+// });
+
+//   useMongoClient: true,
+// }
+/*
 var knex = require('knex')({
   client: 'sqlite3',
   connection: {
@@ -36,5 +130,35 @@ db.knex.schema.hasTable('users').then(function(exists) {
     });
   }
 });
+*/
 
-module.exports = db;
+
+
+
+
+
+// var path = require('path');
+// var knex = require('knex')({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: path.join(__dirname, '../db/shortly.sqlite')
+//   },
+//   useNullAsDefault: true
+// });
+// var db = require('bookshelf')(knex);
+
+
+
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function (user) {
+//       user.increments('id').primary();
+//       user.string('username', 100).unique();
+//       user.string('password', 100);
+//       user.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
